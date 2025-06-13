63f749c18ce2636057f2b3b7c473a700e47bfeda
ChenFY70 <20221481@stu.cqu.edu.cn>
2025-06-14 03:08:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
  
 
 export default defineConfig({
@@ -8,12 +7,5 @@ export default defineConfig({
   server: {
     port: 2999,
     open: true
-  },
-    base: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
   }
-
 })
